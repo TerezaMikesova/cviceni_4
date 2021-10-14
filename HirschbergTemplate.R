b5a5a67558efbb893a024a1725c159e41ec5dda6
Mikešová Tereza (211207) <xmikes13@vutbr.cz>
2021-10-14 13:46:19 +0200

    přidání needle-wunschova algoritmu

--- HirschbergTemplate.R
+++ HirschbergTemplate.R
@@ -1,10 +1,47 @@
+#cv4 - PRG
+#autor: tereza mikešová
+
+NeedleWunsch <- function(sek1,sek2,match,mismatch,gap){
+  m <- 1 + nchar(sek1)
+  n <- 1 + nchar(sek2)
+  sek1 <- strsplit(sek1,split="")
+  sek1 <- sek1[[1]]
+  sek2 <- strsplit(sek2,split="")
+  sek2 <- sek2[[1]]
+  S <- (0:(n-1))*gap
+  
+  for (i in 2:m){
+    s <- S[1]
+    c <- S[1]+gap
+    S[1] <- c
+    
+    for (j in 2:n){
+      if (sek1[i-1] == sek2[j-1]){
+        pom <- match
+      }
+      else{
+        pom <- mismatch}
+      c <- max(c(S[j]+gap, c+gap, s+pom))
+      
+      s <- S[j]
+      S[j] <- c
+      
+    }
+    
+  }
+  return(S)
+  
+}
+
+
+
 Hirschberg_sablona <- function(X,Y,Align,match,mismatch,gap){
   Z <- Align[[1]] #inicializace prvniho radku zarovnani, typ DNAStringSet
   W <- Align[[2]] #inicializace druheho radku zarovnani
   
-  if #delka X je nula
+  if (length(X) == 0)       #delka X je nula
   {
-    for #podle poctu znaku v Y
+    for (i in 1:length(Y))#podle poctu znaku v Y
     {
       Z <- #prida se mezera
         W <- #prida se znak z Y
